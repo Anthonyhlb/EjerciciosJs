05e099287adb2afc26f121d045d6d8b6b18afce8
Anthony <anthonyhildebrand20@gmail.com>
2023-03-01 06:06:00 -0300

    Se completo el ejercicio numero seis

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -10,3 +10,11 @@ let nombre = prompt("Hola nombreUsuario")
 var numero3 = prompt("Escribe un número");
 var numero4 = prompt("Escribe otro número");
 document.write("<br>" + " La suma es: "+ ( parseInt(numero3)+ parseInt(numero4) ) );
+// Ejercicio numero seis
+var numero5 = prompt("Escribe un número");
+var numero6 = prompt("Escribe otro número");
+if (numero5 < numero6) {
+document.write("<br>"+(parseInt(numero5)));
+} else {
+document.write("<br>"+(parseInt(numero6)));
+}
