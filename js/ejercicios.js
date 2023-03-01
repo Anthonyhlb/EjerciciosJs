5a5a4b412f2dd13ca4c1721341f447f011bb7106
Anthony <anthonyhildebrand20@gmail.com>
2023-03-01 05:44:31 -0300

    Se completo el ejercicio numero cinco

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -6,3 +6,7 @@ document.write("Hola mundo");
 let numero1= 3; let numero2= 5; document.write("<br>"+(numero1+numero2))
 // Ejercicio numero cuatro
 let nombre = prompt("Hola nombreUsuario")
+// Ejercicio numero cinco
+var numero3 = prompt("Escribe un número");
+var numero4 = prompt("Escribe otro número");
+document.write("<br>" + " La suma es: "+ ( parseInt(numero3)+ parseInt(numero4) ) );
