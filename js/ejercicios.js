b0605243702789a41624dad18ca1017ad28bad2b
Anthony <anthonyhildebrand20@gmail.com>
2023-03-01 05:27:13 -0300

    Se completo el ejercicio numero cuatro

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,7 +1,8 @@
+// Ejercicio numero uno
 alert("Un mensaje");
-
+// Ejercicio numero dos
 document.write("Hola mundo");
-
-let numero1= 3;
-let numero2= 5;
-document.write("<br>"+(numero1+numero2))
+// Ejercicio numero tres
+let numero1= 3; let numero2= 5; document.write("<br>"+(numero1+numero2))
+// Ejercicio numero cuatro
+let nombre = prompt("Hola nombreUsuario")
